6c2f4c3578e98a911f859e0684c1a978489354d0
sithembisodlamini <sithembisothulane@gmail.com>
2025-03-31 17:50:39 +0200

    Fix main path and include dist

--- dist/random_10_digits.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.getRandom10DigitNumber = getRandom10DigitNumber;
-function getRandom10DigitNumber(length = 10) {
-    return Array.from({ length }, () => "123456789"[Math.floor(Math.random() * 9)]).join("");
-}
